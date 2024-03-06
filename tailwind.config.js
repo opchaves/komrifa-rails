e94d1527b66600089d8b09828c40b2d96e332df5
opchaves <opaulochaves@gmail.com>
2024-03-06 20:25:54 -0300

    add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.(js|jsx|ts|tsx)'
+  ],
+  darkMode: ["class", '[data-theme="dark"]'],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 }
